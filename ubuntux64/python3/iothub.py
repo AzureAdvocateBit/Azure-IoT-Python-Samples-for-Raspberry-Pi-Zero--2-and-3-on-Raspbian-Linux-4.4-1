81be309a93352e0d62a3f4e49b9d1e74c9ba8afc
Dave Glover <dglover@microsoft.com>
2016-09-20 23:20:32 +1000

    Refactored with Classes

---
+++ ubuntux64/python3/iothub.py
@@ -0,0 +1,78 @@
+import iothub_client
+from iothub_client import *
+from iothub_client_args import *
+import sys
+
+
+class IotHub():
+
+    connection_string = ''
+    protocol = IoTHubTransportProvider.AMQP
+    timeout = 241000
+    minimum_polling_time = 9
+
+    # messageTimeout - the maximum time in milliseconds until a message times out.
+    # The timeout period starts at IoTHubClient.send_event_async. 
+    # By default, messages do not expire.
+    message_timeout = 10000
+
+    receive_context = 0
+    message_count = 1
+    received_count = 0
+
+    receive_callbacks = 0
+    send_callbacks = 0
+
+
+    def __init__(self, connectString):
+        self.connection_string = connectString
+
+    def publish(self, msg_txt_formatted, id):
+        message = IoTHubMessage(bytearray(msg_txt_formatted, 'utf8'))
+        self.iotHubClient.send_event_async(message, self.send_confirmation_callback, id)
+
+    def settings(self):
+        print("\nPython %s" % sys.version)
+        print("IoT Hub for Python SDK Version: %s" % iothub_client.__version__)
+        print()
+        print("Protocol %s" % self.protocol)
+        print("Connection string=%s" % self.connection_string)
+        print()
+
+    def send_confirmation_callback(self, message, result, user_context):
+        print("Confirmation[%d] received for message with result = %s" % (user_context, result))
+
+    def initialise(self, messageCallback):
+        self.messageCallback = messageCallback
+        # prepare iothub client
+        iotHubClient = IoTHubClient(self.connection_string, self.protocol)
+        # set the time until a message times out
+        iotHubClient.set_option("messageTimeout", self.message_timeout)
+        iotHubClient.set_message_callback(self.receive_message_callback, self.receive_context)
+        self.iotHubClient = iotHubClient
+        #return iotHubClient
+
+
+    def receive_message_callback(self, message, counter):
+        buffer = message.get_bytearray()
+        size = len(buffer)
+        #print("<<Received Message [%d]>>" % counter)
+        
+        map_properties = message.properties()
+        key_value_pair = map_properties.get_internals()
+
+        self.messageCallback(buffer[:size].decode('utf-8'), key_value_pair)
+
+        return IoTHubMessageDispositionResult.ACCEPTED
+
+
+    def print_last_message_time(self, iotHubClient):
+        try:
+            last_message = iotHubClient.get_last_message_receive_time()
+            print("Last Message: %s" % time.asctime(time.localtime(last_message)))
+            print("Actual time : %s" % time.asctime())
+        except IoTHubClientError as e:
+            if (e.args[0].result == IoTHubClientResult.INDEFINITE_TIME):
+                print("No message received")
+            else:
+                print(e)
--- ubuntux64/python3/openweather.py
+++ ubuntux64/python3/openweather.py
@@ -7,196 +7,53 @@
 # Weather from https://github.com/csparpa/pyowm
 # pip and pip3 install pyowm
 
-
-import random
-import time
-import sys
 import iothub_client
 from iothub_client import *
-from iothub_client_args import *
-import pyowm
-import json
+import time
+import owm 
+import iothub
 
-temperature = 0
-humidity = 0
-pressure = 0
-lastWeatherRequest = time.time() - 60000
-owm = pyowm.OWM('c204bb28a2f9dc23925f27b9e21296dd')  # You MUST provide a valid API key
-
-
-# HTTP options
-# Because it can poll "after 9 seconds" polls will happen effectively
-# at ~10 seconds.
-# Note that for scalabilty, the default value of minimumPollingTime
-# is 25 minutes. For more information, see:
-# https://azure.microsoft.com/documentation/articles/iot-hub-devguide/#messaging
-timeout = 241000
-minimum_polling_time = 9
-
-# messageTimeout - the maximum time in milliseconds until a message times out.
-# The timeout period starts at IoTHubClient.send_event_async. 
-# By default, messages do not expire.
-message_timeout = 10000
-
-receive_context = 0
-message_count = 1
-received_count = 0
-id = 0
-
-# global counters
-receive_callbacks = 0
-send_callbacks = 0
-
-# chose HTTP, AMQP or MQTT as transport protocol
-protocol = IoTHubTransportProvider.AMQP
-
-# String containing Hostname, Device Id & Device Key in the format:
-# "HostName=<host_name>;DeviceId=<device_id>;SharedAccessKey=<device_key>"
-connection_string = "HostName=IoTCampAU.azure-devices.net;DeviceId=UbuntuSB4;SharedAccessKey=ATLRGx7qgUOwCIT1o3l2XJTidEyQmD7fSpKGZfrGViw="
-
-msg_txt = "{\"Geo\": \"Sydney\",\"Humidity\":%d,\"HPa\":%d,\"Light\":%d,\"Celsius\": %.2f,\"Id\":%d}"
-
-def getWeather():
-    global temperature
-    global humidity
-    global pressure
-    global lastWeatherRequest
-
-    try:
-        if lastWeatherRequest + 60 < time.time():
-            lastWeatherRequest = time.time()
-            
-            observation = owm.weather_at_place('Sydney,au')
-            w = observation.get_weather()
-
-            temp = w.get_temperature('celsius')
-            humidity = w.get_humidity()
-            press = w.get_pressure()
-
-            j = json.dumps(temp)
-            o = json.loads(j)
-            temperature = o['temp']
-
-            j = json.dumps(press)
-            o = json.loads(j)
-            pressure = o['press']
-    except:
-        print("open weather call failed")
+sensorLocation = "Sydney"
+owmLocation = 'Sydney,AU'
+openWeather = owm.Weather('c204bb28a2f9dc23925f27b9e21296dd', owmLocation)
+iot = iothub.IotHub('HostName=IoTCampAU.azure-devices.net;DeviceId=UbuntuSB4;SharedAccessKey=ATLRGx7qgUOwCIT1o3l2XJTidEyQmD7fSpKGZfrGViw=')
+msg_txt = "{\"Geo\":%s,\"Humidity\":%d,\"HPa\":%d,\"Celsius\": %.2f,\"Id\":%d}"
 
 
-# some embedded platforms need certificate information
-def set_certificates(iotHubClient):
-    from iothub_client_cert import certificates
-    try:
-        iotHubClient.set_option("TrustedCerts", certificates)
-        print("set_option TrustedCerts successful")
-    except IoTHubClientError as e:
-        print("set_option TrustedCerts failed (%s)" % e)
-
-
-def receive_message_callback(message, counter):
-    global receive_callbacks
-    buffer = message.get_bytearray()
-    size = len(buffer)
-    print("Received Message [%d]:" % counter)
-    print("    Data: <<<%s>>> & Size=%d" % (buffer[:size].decode('utf-8'), size))
-    map_properties = message.properties()
-    key_value_pair = map_properties.get_internals()
-    print("    Properties: %s" % key_value_pair)
-    counter += 1
-    receive_callbacks += 1
-    print("    Total calls received: %d" % receive_callbacks)
-    return IoTHubMessageDispositionResult.ACCEPTED
-
-
-def send_confirmation_callback(message, result, user_context):
-    global send_callbacks
-    print("Confirmation[%d] received for message with result = %s" % (user_context, result))
-
-
-def iothub_client_init():
-    # prepare iothub client
-    iotHubClient = IoTHubClient(connection_string, protocol)
-    if iotHubClient.protocol == IoTHubTransportProvider.HTTP:
-        iotHubClient.set_option("timeout", timeout)
-        iotHubClient.set_option("MinimumPollingTime", minimum_polling_time)
-    # set the time until a message times out
-    iotHubClient.set_option("messageTimeout", message_timeout)
-    # some embedded platforms need certificate information
-    # set_certificates(iotHubClient)
-    # to enable MQTT logging set to 1
-    if iotHubClient.protocol == IoTHubTransportProvider.MQTT:
-        iotHubClient.set_option("logtrace", 0)
-    iotHubClient.set_message_callback(
-        receive_message_callback, receive_context)
-    return iotHubClient
-
-
-def print_last_message_time(iotHubClient):
-    try:
-        last_message = iotHubClient.get_last_message_receive_time()
-        print("Last Message: %s" % time.asctime(time.localtime(last_message)))
-        print("Actual time : %s" % time.asctime())
-    except IoTHubClientError as e:
-        if (e.args[0].result == IoTHubClientResult.INDEFINITE_TIME):
-            print("No message received")
-        else:
-            print(e)
+def callback(message, properties):
+    print(message)
+    print("Properties: %s" % properties)
 
 
 def iothub_client_sample_run():
-    global id    
+    id = 0  
 
-    iotHubClient = iothub_client_init()
+    iot.initialise(callback)
 
     while True:
         try:
-            getWeather()
-            ## normalise light to something of 100%
-            lightLevel = 500
-            if lightLevel > 1024:
-                lightLevel = 1024            
-            lightLevel = lightLevel * 100 / 1024
-
+            openWeather.getWeather()
             id += 1
 
-            msg_txt_formatted = msg_txt % (humidity, pressure, 50, temperature, id)
-
-            message = IoTHubMessage(bytearray(msg_txt_formatted, 'utf8'))
-        
-            iotHubClient.send_event_async(message, send_confirmation_callback, id)
+            msg_txt_formatted = msg_txt % (sensorLocation, openWeather.humidity, openWeather.pressure, openWeather.temperature, id)
+            iot.publish(msg_txt_formatted, id)
             
-            time.sleep(10)
+            time.sleep(4)
 
         except IoTHubError as e:
             print("Unexpected error %s from IoTHub" % e)
-            print_last_message_time(iotHubClient)
-            time.sleep(10)
+            print_last_message_time(iot.iotHubClient)
+            time.sleep(4)
         
         except KeyboardInterrupt:
             print("IoTHubClient sample stopped")
             return
 
 
-def usage():
-    print("Usage: iothub_client_sample.py -p <protocol> -c <connectionstring>")
-    print("    protocol        : <amqp, http, mqtt>")
-    print("    connectionstring: <HostName=<host_name>;DeviceId=<device_id>;SharedAccessKey=<device_key>>")
-
-
 if __name__ == '__main__':
-    print("\nPython %s" % sys.version)
-    print("IoT Hub for Python SDK Version: %s" % iothub_client.__version__)
+    iot.settings()
 
-    try:
-        (connection_string, protocol) = get_iothub_opt(sys.argv[1:], connection_string, protocol)
-    except OptionError as o:
-        print(o)
-        usage()
-        sys.exit(1)
-
-    print("Starting the IoT Hub Python sample...")
-    print("    Protocol %s" % protocol)
-    print("    Connection string=%s" % connection_string)
+    print("Starting the Open Weather Map Azure IoT Hub Python sample...")
+    print()
 
     iothub_client_sample_run()
