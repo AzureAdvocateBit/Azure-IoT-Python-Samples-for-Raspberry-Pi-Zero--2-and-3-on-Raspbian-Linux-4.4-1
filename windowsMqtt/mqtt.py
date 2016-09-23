35c40f6705c9df72abce83a403241cff6d246484
Dave Glover <dglover@microsoft.com>
2016-09-23 16:09:11 +1000

    Added Sas generator and tls cert

--- windowsMqtt/mqtt.py
+++ windowsMqtt/mqtt.py
@@ -1,53 +1,72 @@
-# HostName=IoTCampAU.azure-devices.net;DeviceId=mqtt;SharedAccessSignature=SharedAccessSignature sr=IoTCampAU.azure-devices.net%2fdevices%2fmqtt&sig=tQz78YxYsyRCDCTOPGMIrYFhYByPc26FCfCfB%2b6HgBI%3d&se=1491831743
 # https://azure.microsoft.com/en-us/documentation/articles/iot-hub-mqtt-support/
 # http://stackoverflow.com/questions/35452072/python-mqtt-connection-to-azure-iot-hub/35473777
-# https://github.com/bechynsky/AzureIoTDeviceClientPY
 
 
 import paho.mqtt.client as mqtt
+import time
+from datetime import datetime, timedelta
+import base64
+import hmac
+import urllib.parse
 
 
 hubAddress = 'IoTCampAU.azure-devices.net'
 hubName = 'mqtt'
-hubPass = 'SharedAccessSignature sr=IoTCampAU.azure-devices.net%2fdevices%2fmqtt&sig=o7YAZr8jJYITgFKG1fIVi2pIfmof%2bTHSHCmYCky%2fzJE%3d&se=1478873518'
+SharedAccessKey= 'VZbmLwYjjdg04Gb5SvbNMTq44GdDEO5k5px6OUIu7l8='
+endpoint = hubAddress + '/devices/' + hubName
+
 hubUser = hubAddress + '/' + hubName
 hubTopicPublish = 'devices/' + hubName + '/messages/events/'
 hubTopicSubscribe = 'devices/' + hubName + '/messages/devicebound/#'
 
+# sas generator from https://github.com/bechynsky/AzureIoTDeviceClientPY/blob/master/DeviceClient.py
+def generate_sas_token(uri, key, expiry=4320000): # 50 day expiry
+    ttl = int(time.time()) + expiry
 
+    urlToSign = urllib.parse.quote(uri, safe='') 
+    sign_key = "%s\n%d" % (urlToSign, int(ttl))
+    h = hmac.new(base64.b64decode(key), msg = "{0}\n{1}".format(urlToSign, ttl).encode('utf-8'),digestmod = 'sha256')
+
+    signature = urllib.parse.quote(base64.b64encode(h.digest()), safe = '')
+    return "SharedAccessSignature sr={0}&sig={1}&se={2}".format(urlToSign, urllib.parse.quote(base64.b64encode(h.digest()), safe = ''), ttl)
 
 def on_connect(client, userdata, flags, rc):
-    print( "Connected with result code: %s" % rc)
+    print("Connected with result code: %s" % rc)
     client.subscribe(hubTopicSubscribe)
-    client.publish(hubTopicPublish, "test")
-
 
 def on_disconnect(client, userdata, rc):
-    print( "Disconnected with result code: %s" % rc)
-
+    print("Disconnected with result code: %s" % rc)
 
 def on_message(client, userdata, msg):
-    print( " - ".join((msg.topic, str(msg.payload))))
+    print(" - ".join((msg.topic, str(msg.payload))))
     # Do this only if you want to send a reply message every time you receive one
-    client.publish("devices/<YOUR DEVICE ID>/messages/events", "REPLY", qos=1)
-
+    #client.publish("devices/mqtt/messages/events", "REPLY", qos=1)
 
 def on_publish(client, userdata, mid):
     print("Sent message")
+
+def publish():
+    while True:
+        client.publish(hubTopicPublish, "test")
+        time.sleep(2)
 
 
 client = mqtt.Client(hubName, mqtt.MQTTv311)
 
 client.on_connect = on_connect
 client.on_disconnect = on_disconnect
-#client.on_message = on_message
-#client.on_publish = on_publish
+client.on_message = on_message
+client.on_publish = on_publish
 
-client.username_pw_set(hubUser, hubPass)
-client.tls_insecure_set(True) # You can also set the proper certificate using client.tls_set()
+#client.username_pw_set(hubUser, hubPass)
+client.username_pw_set(hubUser, generate_sas_token(endpoint, SharedAccessKey))
 
-client.connect_async(hubAddress, 8883)
+#client.tls_set("ca-certificates.crt") # use builtin cert on Raspbian
+client.tls_set("baltimoreBase64.cer") # Baltimore Cybertrust Root exported using certlm.msc in base64 format
+client.connect(hubAddress, 8883)
 
 
-client.loop_forever()
+#client.loop_forever()
+client.loop_start()
 
+publish()
